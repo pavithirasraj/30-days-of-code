c0e0594909ed24844a8054dc1ee5efb4aeeb027d
Pavithira S <126556090+pavithirasraj@users.noreply.github.com>
2023-07-07 23:18:58 +0530

    Create string-slicing.py

---
+++ string-slicing.py
@@ -0,0 +1,29 @@
+#Slicing
+
+s = input().strip()
+# Reversing a string
+new_s = s[::-1]
+print(new_s)
+
+# Print the last k values of a string
+s = input().strip()
+k = int(input())
+new_s = s[::-1]
+# abc ==> cba | k = 2 ==> cb
+print(new_s[:k])
+
+# Skip last k values of a string and print the rest
+s = input().strip()
+k = int(input())
+new_s = s[::-1]
+print(new_s[k:])
+
+# Print first k values of a string
+s=input().strip()
+k=int(input())
+print(s[:k])
+
+# Skip first k values of a string and print the rest
+s=input().strip()
+k=int(input())
+print(s[k:])
